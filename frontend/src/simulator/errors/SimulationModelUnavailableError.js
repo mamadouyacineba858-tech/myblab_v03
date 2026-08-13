622a9f810bc7bb2c56f7f2de39fabb62b3fe6874
mamadouyacineba858-tech <mamadouyacineba858@gmail.com>
2026-08-13 18:35:17 +0200

    feat(simulator): add unavailable model error

---
+++ frontend/src/simulator/errors/SimulationModelUnavailableError.js
@@ -0,0 +1,7 @@
+export class SimulationModelUnavailableError extends Error {
+  constructor(type) {
+    super(`SimulationModelUnavailableError: "${type}" is declared in the canonical registry but has no simulation model available (modelAvailable = false). This component exists visually/declaratively but cannot be simulated yet.`)
+    this.name = 'SimulationModelUnavailableError'
+    this.type = type
+  }
+}
