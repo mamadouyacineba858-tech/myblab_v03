aa4e72208116a12741f51a0ef5347ee56e473153
mamadouyacineba858-tech <mamadouyacineba858@gmail.com>
2026-08-13 18:58:17 +0200

    add simulation registry error barrel

---
+++ frontend/src/simulator/errors/index.js
@@ -0,0 +1,4 @@
+export { UnknownComponentTypeError } from './UnknownComponentTypeError.js'
+export { SimulationModelUnavailableError } from './SimulationModelUnavailableError.js'
+export { InvalidSimulationModelError } from './InvalidSimulationModelError.js'
+export { UnsupportedSimulationCapabilityError } from './UnsupportedSimulationCapabilityError.js'
