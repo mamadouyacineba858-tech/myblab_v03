cc9245a9ba5b3a7181dfd8aa6e2de3e538551655
mamadouyacineba858-tech <mamadouyacineba858@gmail.com>
2026-08-13 18:35:08 +0200

    feat(simulator): add MB-CF2-SIM-001 unknown type error

---
+++ frontend/src/simulator/errors/UnknownComponentTypeError.js
@@ -0,0 +1,7 @@
+export class UnknownComponentTypeError extends Error {
+  constructor(type) {
+    super(`UnknownComponentTypeError: "${type}" is not declared in the canonical registry (canonicalRegistry.js). This type does not exist for the platform, not even declaratively.`)
+    this.name = 'UnknownComponentTypeError'
+    this.type = type
+  }
+}
